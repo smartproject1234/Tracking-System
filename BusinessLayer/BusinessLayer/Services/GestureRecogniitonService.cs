9933793e89af09a867642cf8b5b6f10b643aa6f9
vüsal <vsal@LAPTOP-8PPT5PTN>
2024-09-28 15:08:15 +0400

    Services were Created

---
+++ BusinessLayer/BusinessLayer/Services/GestureRecogniitonService.cs
@@ -0,0 +1,33 @@
+﻿using Emgu.CV;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services
+{
+    public class GestureRecogniitonService
+    {
+        private CascadeClassifier handCascade;
+
+        public GestureRecogniitonService()
+        {
+            handCascade = new CascadeClassifier("haarcascade_hand.xml");
+
+            //Jest için
+    
+        }
+
+        public bool RecognizedeGesture(Mat frame)
+        {
+            var grayImage = new UMat();
+            CvInvoke.CvtColor(frame, grayImage, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
+
+            var hands = handCascade.DetectMultiScale(grayImage, 1.1, 10, Size.Empty);
+
+            return hands.Length > 0;
+        }
+    }
+
+}
